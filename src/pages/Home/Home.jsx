a693ca02854f2a3a17cec7ecee5c8653eedc5c34
Abdelmoneim21 <abdelmonemramadan9@gmail.com>
2024-11-20 03:33:25 +0200

    Menu has been added

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,11 @@
+import ExploreMenu from "../../components/ExploreMenu/ExploreMenu";
 import Header from "../../components/Header/Header";
 
 export default function Home(){
     return(
         <div className="home">
             <Header />
+            <ExploreMenu />
         </div>
     )
 }
